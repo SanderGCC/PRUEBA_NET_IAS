fdaea8fb502e689ad21a9a9cb7aeb39970cdb171
SanderGCC <sandergcc@gmail.com>
2024-05-31 12:24:04 -0500

    init

---
+++ V2/Data/AppDBContext.cs
@@ -0,0 +1,29 @@
+﻿
+namespace V2.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using V2.Models;
+
+    public class ApplicationDbContext : DbContext
+    {
+        /// <summary>
+        /// Constructor de la clase.
+        /// </summary>
+        /// <param name="options"></param>
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// DbSet de la entidad Automoviles
+        /// </summary>
+        public DbSet<Automoviles> Automoviles { get; set; }
+
+
+        /// <summary>
+        /// DbSet de la entidad Marcas
+        /// </summary>
+        public DbSet<Marcas> Marcas { get; set; }
+    }
+
+}
---
+++ V2/Models/Automoviles.cs
@@ -0,0 +1,45 @@
+﻿
+namespace V2.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Automoviles
+    {
+
+        [Key]
+        /// <summary>
+        /// Define la llave primaria de la entidad Automovil.
+        /// </summary>
+        public int Id { get; set; }
+
+        [Required]
+        /// <summary>
+        /// Atributo Modelo.
+        /// </summary>
+        public string Modelo { get; set; }
+
+        /// <summary>
+        /// Atributo Descripción.
+        /// </summary>
+        public string? Descripción { get; set; }
+
+        [Required]
+        /// <summary>
+        /// Atributo Precio.
+        /// </summary>
+        public int Precio { get; set; }
+
+        [Required]
+        /// <summary>
+        /// Atributo Kilometraje.
+        /// </summary>
+        public decimal Kilometraje { get; set; }
+
+        [Required]
+        /// <summary>
+        /// Atributo que define la relación con la tabla "Marca".
+        /// </summary>
+        public int IdMarca { get; set; }
+    }
+
+}
